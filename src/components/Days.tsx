8f6f1b34f0575037bd33c565c0628c84f47095e4
Luiz Franzon <luizfranzon05@hotmail.com>
2023-09-11 16:45:36 -0300

    Update Days.tsx

--- src/components/Days.tsx
+++ src/components/Days.tsx
@@ -4,12 +4,19 @@ export function Days() {
   const now = dayjs()
   const libertyDay = dayjs('2023-10-12')
 
-  const daysRemaining = libertyDay.diff(now, 'day')
+  let cardText 
+  if (libertyDay.diff(now, 'day') === 0) {
+    cardText = "Hoje acaba!"
+  } else if (libertyDay.diff(now, 'day') === 1) {
+    cardText = "Amanhã acaba!"
+  } else {
+    cardText = libertyDay.diff(now, 'day')
+  }
 
   return (
     <div className="fixed cursor-progress top-9 bg-white/10 px-12 py-4 rounded-xl">
       <span className="font-bold text-4xl text-white">
-        {daysRemaining} dias restantes
+        {cardText} dias restantes
       </span>
     </div>
   )
